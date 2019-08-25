6331738f3f2f2eda83657979df8404ead1346dd5
Deadpool-BlackPanther <38010158+Deadpool-BlackPanther@users.noreply.github.com>
2019-08-25 13:22:07 +0530

    Create MissingNo.java

---
+++ MissingNo.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+    
+    
+	public static void main(String[] args) {
+		int arr[] = {2,5,3,6,1};
+		int sum =0;
+		sum = (6*(7))/2;
+		for(int i=0; i<arr.length;i++)
+		{
+		    sum-=arr[i];
+		}
+		System.out.println("Ans is"+sum);	}
+}
